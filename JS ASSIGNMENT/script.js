733e7d700db50e2451b3a9e3a73dae276221152d
aarif1129 <aarifbhatt1129@gmail.com>
2023-12-30 00:15:55 +0530

    Projects

---
+++ JS ASSIGNMENT/script.js
@@ -0,0 +1,27 @@
+ class restaurantmanager{
+    restaurantlist=[{
+        id:1,
+        restaurantName:'KFC',
+        address:'Anand Vihar',
+        city:'Delhi',
+    },
+    {id:2,
+        restaurantName:'Domino',
+        address:'Savita Vihar',
+        city:'Delhi',
+    }, 
+     {id:3,
+        restaurantName:'Burger King',
+        address:'Civil Lines',
+        city:'Pune',
+     },
+    {id:4,
+        restaurantName:'Subway',
+        address:'Cantoment',
+        city:'Mumbai',
+ }];
+    
+        printAllRestaurantNames=()=>{
+
+        }
+ }
